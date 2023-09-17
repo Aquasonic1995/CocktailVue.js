44966272240bc5870b12b2022579802c7132e9eb
Aquasonic1995 <Aquasonic1995@gmail.com>
2023-09-17 09:23:12 +0300

    feat:add http request for cocktails

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,36 +1,45 @@
 <template>
-  <app-layout imgUrl="/src/assets/img/bg-home.jpg" >
+  <app-layout imgUrl="/src/assets/img/bg-home.jpg">
     <div class="wrapper">
       <div class="title">Choose your drink</div>
-      <el-select class="select" v-model="ingredient" placeholder="Choose main ingredient">
+      <el-select class="select"
+                 v-model="ingredient"
+                 placeholder="Choose main ingredient"
+                 @change="store.getCocktails(ingredient)"
+      >
         <el-option
             v-for="item in ingredients"
             :key="item.strIngredient1"
             :label="item.strIngredient1"
             :value="item.strIngredient1"
-
         />
       </el-select>
-      <div class="promo"><p>Try our delicious cocktail recipes for every occasion. Find delicious cocktail recipes by ingredient through our
+      <div class="promo"><p>Try our delicious cocktail recipes for every occasion. Find delicious cocktail recipes by
+        ingredient through our
         <br> cocktail generator.</p></div>
+      <img src="/Cocktails.png" alt="Cocktails">
     </div>
+
 
   </app-layout>
 </template>
 
 <script setup>
 import {onMounted, ref} from "vue";
+
 const store = useCounterStore()
 onMounted(store.getIngredients)
-const {ingredients} = storeToRefs((store))
+const {ingredients, cocktails} = storeToRefs((store))
 const ingredient = ref(null)
 import AppLayout from "@/components/AppLayout.vue";
 import {useCounterStore} from "@/stores/root";
 import {storeToRefs} from "pinia";
+
 </script>
 
 <style lang="scss" scoped>
 @import "@/assets/styles/variables";
+
 .select {
   margin-left: auto;
   margin-right: auto;
@@ -39,11 +48,18 @@ import {storeToRefs} from "pinia";
   border: 1px solid $text;
   margin-bottom: 60px;
 }
-.promo{
+
+.promo {
   text-align: center;
-  color:$textMuted;
+  color: $textMuted;
   letter-spacing: 0.1em;
   line-height: 36px;
+  margin-bottom: 60px;
 }
 
+img {
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 345px;
+}
 </style>
--- src/stores/root.ts
+++ src/stores/root.ts
@@ -1,18 +1,23 @@
-
-import { defineStore } from 'pinia'
+import {defineStore} from 'pinia'
 
 import axios from "axios";
 
 export const useCounterStore = defineStore('root', {
-state:()=>({
-ingredients:[]
-}),
-  actions:{
-  async getIngredients(){
-    const data = await axios.get("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
-    )
-    this.ingredients=data?.data?.drinks
-  }
-  }
+    state: () => ({
+        ingredients: [],
+        cocktails: []
+    }),
+    actions: {
+        async getIngredients() {
+            const data = await axios.get("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+            )
+            this.ingredients = data?.data?.drinks
+        },
+        async getCocktails(ingredient: any) {
+            const data = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`
+            )
+            this.cocktails = data?.data?.drinks
+        }
+    }
 
-} )
+})
